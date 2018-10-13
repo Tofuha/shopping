c2ab0c0242d810e35c85cc6c3be65965bab8495a
tofuha <jjliew@protonmail.com>
2018-10-13 14:08:49 +0200

    Updates

---
+++ precache-manifest.4e819d938ffc90280932f4ac5ee5e453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c43f3f98e252583d0a",
+    "url": "/shopping/static/js/runtime~main.c4c43f3f.js"
+  },
+  {
+    "revision": "e4997f24af0d7659c46e",
+    "url": "/shopping/static/js/main.e4997f24.chunk.js"
+  },
+  {
+    "revision": "898312dd0c16e1973c08",
+    "url": "/shopping/static/js/1.898312dd.chunk.js"
+  },
+  {
+    "revision": "e4997f24af0d7659c46e",
+    "url": "/shopping/static/css/main.b3306988.chunk.css"
+  },
+  {
+    "revision": "898312dd0c16e1973c08",
+    "url": "/shopping/static/css/1.90ff6a76.chunk.css"
+  },
+  {
+    "revision": "562035eddd582c9fc86567dc5d6c4c3a",
+    "url": "/shopping/index.html"
+  }
+];
